2482afe41873f2e0dd1ec076db633006948025d8
James Spurin <james@spurin.com>
2024-07-30 16:08:09 +0100

    update multi-arch support

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 docker buildx create --name build_cross --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=10000000 --driver-opt env.BUILDKIT_STEP_LOG_MAX_SPEED=10000000
 docker buildx use build_cross
-#docker buildx build --platform linux/386,linux/amd64,linux/arm64/v8,linux/arm/v6,linux/arm/v7,linux/ppc64le,linux/s390x -t spurin/fake-lb:latest . --push
-docker buildx build --platform linux/amd64,linux/arm64/v8 -t spurin/fake-lb:latest . --push
+docker buildx build --platform linux/amd64,linux/arm64/v8,linux/arm/v6,linux/arm/v7 -t spurin/fake-lb:latest . --push
